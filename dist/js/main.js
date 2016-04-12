afb3374d8c3cb37062424bd94f9d814b0c315377
Mateus Fernandes <mf.silvalima@gmail.com>
2016-04-12 16:53:38 -0300

    insert json for letters

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,40 +11,41 @@ function animacoes(){
   });
 }
 
-function wordFunction() {
-		var imgs = {
-      a : ["https://uploaddeimagens.com.br/images/000/595/986/original/a1.jpg", "https://uploaddeimagens.com.br/images/000/595/993/original/a2.jpg"],
-      b : ["https://uploaddeimagens.com.br/images/000/595/996/original/b1.jpg", "https://uploaddeimagens.com.br/images/000/595/998/original/b2.jpg"]
-    };
-    var word = document.querySelector("#word").value.toLowerCase();
-    var wordLength = word.length;
-    var canvas = document.querySelector('#wordCanvas');
-    var context = canvas.getContext('2d');
+function drawCanvas(imgs) {
+  var word = document.querySelector("#word").value.toLowerCase();
+  var wordLength = word.length;
+  var canvas = document.querySelector('#wordCanvas');
+  var context = canvas.getContext('2d');
 
-    canvas.width = wordLength * 250;
-    canvas.height = 250;
+  canvas.width = wordLength * 250;
+  canvas.height = 250;
 
-    var i;
-    for (i = 0; i < word.length; i++) {
-    	var letter = word[i];
-      var letterArray = imgs[letter];
-      var letterImage = letterArray[Math.floor( Math.random() * letterArray.length )];
-      var imageObj = new Image();
-      imageObj.src = letterImage;
-     	imageObj.position = i * 250;
-        imageObj.onload = function() {
-         context.drawImage(this, 0, 0, 960, 960, this.position, 0, 250, 250);
-       };
-    }
+  var i;
+  for (i = 0; i < word.length; i++) {
+    var letter = word[i];
+    var letterArray = imgs[letter];
+    var letterImage = letterArray[Math.floor( Math.random() * letterArray.length )];
+    var imageObj = new Image();
+    imageObj.src = letterImage;
+    imageObj.position = i * 250;
+      imageObj.onload = function() {
+       context.drawImage(this, 0, 0, 960, 960, this.position, 0, 250, 250);
+     };
+  }
 }
 
+function loadJsonLetters() {
+  $.getJSON( "../../letters.json", function(data) {
+    drawCanvas(data);
+  })
+}
 
 $(document).on('ready', function () {
     $(window).on('scroll', function(){
       animacoes();
     });
-    $("#form").submit('handler', function() {
-      wordFunction();
+    $("#form").submit(function() {
+      loadJsonLetters();
       return false;
     });
 });
